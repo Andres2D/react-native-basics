06954b9579e7757e011133cb4b79f6b7ad234f48
Andres2D <andres2d1997.1206@gmail.com>
2022-01-24 18:25:57 -0500

    Add example of types on TS

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export const BasicTypes = () => {
-  return (
-    <>
-        <h3>Basic Types</h3>
-    </>
-  );
+
+    const name: string = 'Andres';
+    const age: number = 35;
+    const isActive: boolean = false;
+
+    const powers: string[] = ['Velocity', 'Fly', 'Breath on weather']
+
+    return (
+        <>
+            <h3>Basic Types</h3>
+            {name}, {age}, {(isActive) ? 'active' : 'not active'}
+            <br />
+            {powers.join(', ')}
+        </>
+    );
 };
