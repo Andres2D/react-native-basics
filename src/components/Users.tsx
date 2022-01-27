8fe88fa2263ef5ff1a949e8eb5f69d0e01a30605
Andres2D <andres2d1997.1206@gmail.com>
2022-01-27 14:23:21 -0500

    adding table and pagitation

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { reqResApi } from '../api/req-res';
 import { ReqResList, User } from '../interfaces/reqRes';
 
@@ -6,16 +6,47 @@ export const Users = () => {
 
     const [users, setUsers] = useState<User[]>([]);
 
+    const pageRef = useRef(1);
+
     useEffect(() => {
-        //call API
-        reqResApi.get<ReqResList>('/users')
-            .then( resp => {
-                console.log(resp);
-                console.log(resp.data.data);
-            })
-            .catch(console.log);
+        loadUsers();
     }, []);
+
+    const loadUsers = async() => {
+        //call API
+        const resp = await reqResApi.get<ReqResList>('/users', {
+            params: {
+                page: pageRef.current
+            }
+        });
+        
+        if(resp.data.data.length > 0) {
+            setUsers(resp.data.data);
+            pageRef.current ++;
+        }else{
+            alert('No more pages')
+        }
+    }
     
+    const renderItem = ({id, first_name, last_name, email, avatar}: User) => {
+        return (
+            <tr key={ id.toString() }>
+                <th>
+                    <img 
+                        src={avatar} 
+                        alt={first_name}
+                        style={{
+                            width: 35,
+                            borderRadius: 100
+                        }}
+                        />
+                </th>
+                <th>{first_name} {last_name}</th>
+                <th>{email}</th>
+            </tr>
+        )
+    }
+
 
   return (
       <>
@@ -29,8 +60,17 @@ export const Users = () => {
                 </tr>
             </thead>
             <tbody>
+                {
+                    users.map( renderItem )
+                }
             </tbody>
         </table>
+        <button
+            className='btn btn-primary'
+            onClick={ loadUsers }
+        >
+            Nexts
+        </button>
       </>
   )
 };
